fa51dd7af2e71edbc99f6180961a196d26873374
George Liu <eva2000@centminmod.com>
2015-02-23 19:01:23 +1000

    add wrk and wrk2 http load testing tools

--- siegesetup.sh
+++ siegesetup.sh
@@ -3,6 +3,9 @@
 # siege install for docker containers
 # based on centminmod.com install routine
 # by George Liu vbtechsupport.com
+# http://www.joedog.org/siege-home/
+# https://github.com/wg/wrk.git
+# https://github.com/giltene/wrk2
 ###############################################
 SIEGE_VERSION=latest
 DIR_TMP=/usr/local/src
@@ -11,6 +14,8 @@ SIEGE_LINKFILE="siege-${SIEGE_VERSION}.tar.gz"
 SIEGE_LINK="http://download.joedog.org/siege/${SIEGE_LINKFILE}"
 SPROXY_LINKFILE="sproxy-latest.tar.gz"
 SPROXY_LINK="http://download.joedog.org/sproxy/${SPROXY_LINKFILE}"
+WRK_LINK='https://github.com/wg/wrk.git'
+WRKTWO_LINK='https://github.com/giltene/wrk2.git'
 ###############################################
 install() {
 cd $DIR_TMP
@@ -33,6 +38,18 @@ cd sproxy-1*
 ./configure
 make -j2
 make install
+
+cd $DIR_TMP
+git clone $WRK_LINK
+cd wrk
+make
+\cp -f wrk /usr/bin/wrk
+
+cd $DIR_TMP
+git clone $WRKTWO_LINK
+cd wrk2
+make
+\cp -f wrk /usr/bin/wrk2
 }
 install
 exit
