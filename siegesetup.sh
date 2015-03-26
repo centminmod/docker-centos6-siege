cb16ae79c5e2de8fedcee9f81867e999dddb81e5
George Liu <eva2000@centminmod.com>
2015-03-26 22:23:30 +1000

    fix httperf install via github

--- siegesetup.sh
+++ siegesetup.sh
@@ -11,6 +11,9 @@
 ###############################################
 SIEGE_VERSION=3.0.9
 DIR_TMP=/usr/local/src
+
+SSLDIR_TMP=/usr/local/src/docker_openssl
+STATICLIBSSL=/opt/docker_openssl
 ###############################################
 SIEGE_LINKFILE="siege-${SIEGE_VERSION}.tar.gz"
 SIEGE_LINK="http://download.joedog.org/siege/${SIEGE_LINKFILE}"
@@ -22,9 +25,61 @@ WEIGHTTP='http://cgit.lighttpd.net/weighttp.git/snapshot/weighttp-master.tar.gz'
 SLOWHTTP='https://github.com/shekyan/slowhttptest.git'
 HTTPERF_LINK='https://github.com/httperf/httperf.git'
 ###############################################
-install() {
-cd $DIR_TMP
+#
 
+if [ -f /proc/user_beancounters ]; then
+    CPUS=`cat "/proc/cpuinfo" | grep "processor"|wc -l`
+    MAKETHREADS=" -j$CPUS"
+else
+    # speed up make
+    CPUS=`cat "/proc/cpuinfo" | grep "processor"|wc -l`
+    MAKETHREADS=" -j$CPUS"
+fi
+
+staticssl() {
+	# Build static openssl
+	if [ ! -d "$SSLDIR_TMP" ]; then
+		# mkdir $SSLDIR_TMP
+		cd $DIR_TMP
+		if [ -d ${DIR_TMP}/docker_openssl ]; then
+			rm -rf ${DIR_TMP}/docker_openssl
+		fi
+		git clone --depth 1 https://github.com/PeterMosmans/openssl.git docker_openssl -b 1.0.2-chacha
+	fi
+
+    rm -rf "$STATICLIBSSL"
+    mkdir -p "$STATICLIBSSL"
+    # cd openssl-${OPENSSL_VERSION}
+    cd $SSLDIR_TMP
+    make clean
+    # enable-ec_nistp_64_gcc_128 option only supported in 64bit linux
+    if [[ "$(uname -m)" = 'x86_64' ]]; then
+    	./config --prefix=${STATICLIBSSL} --openssldir=${STATICLIBSSL}/ssl enable-threads zlib shared experimental-jpake enable-md2 enable-rc5 enable-rfc3779 enable-gost enable-static-engine enable-ec_nistp_64_gcc_128
+	else
+		./config --prefix=${STATICLIBSSL} --openssldir=${STATICLIBSSL}/ssl enable-threads zlib shared
+	fi
+    # make depend
+    make${MAKETHREADS}
+    make install
+
+    echo
+    echo "OpenSSL static prefix ${STATICLIBSSL}"
+    echo "openssldir ${STATICLIBSSL}/ssl"
+    /opt/docker_openssl/bin/openssl version
+    
+    echo
+    echo "ls -lah ${STATICLIBSSL}"
+    ls -lah ${STATICLIBSSL}
+    echo
+}
+
+yumpkg() {
+	yum -y install libtool autoconf automake make gcc libevent libevent-devel
+}
+
+install() {
+
+cd $DIR_TMP
 wget -cnv ${SIEGE_LINK}
 tar -xzf siege-${SIEGE_VERSION}.tar.gz
 rm -rf siege-${SIEGE_VERSION}.tar.gz
@@ -74,13 +129,18 @@ make install
 
 cd $DIR_TMP
 git clone $HTTPERF_LINK httperf
-cd httperf
+cd ${DIR_TMP}/httperf
+autoreconf -i
 mkdir build
 cd build
-../configure
+# CPPFLAGS="-I${STATICLIBSSL}/include" LDFLAGS="-L${STATICLIBSSL}/lib" ../configure --enable-idleconn
+../configure --enable-idleconn
 make
 make install
 
 }
+###########################
+yumpkg
+#staticssl
 install
 exit
